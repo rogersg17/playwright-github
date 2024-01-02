fddb4b18f68da1053eb69c5d715a57b111224eb8
Garry <garry.rogers@titanium.solutions>
2024-01-02 13:13:46 +0000

    added check for installing playwright header

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -17,6 +17,9 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
+  // Expects page to have a link with the name Installing Playwright.
+  await expect(page.getByRole('heading', { name: 'Installing Playwright' })).toBeVisible();
 });
 });
 
